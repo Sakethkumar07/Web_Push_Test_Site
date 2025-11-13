b1f1c6c901f8b45febb916f7901e5ad4333aa3a5
Sakethkumar07 <119095790+Sakethkumar07@users.noreply.github.com>
2025-11-13 15:28:18 +0530

    Add files via upload

---
+++ webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BANiuKhD5A1LnKVj5NKwMOFGl2FQb3iXCvpcUr8wCn1CsQ7ExXu7g1faeAN9iGzMttGcs_ndvivf7nuGB3c8ok0=","apiKey":"ABET9SE5tiSUYclUQy62SkN7A","accountToken":"ABElKaqpK-3l_zDeOp-XArlkg","appver":"0.0.0","apiHost":"https://abr16c0-webpush.oraclersys.com","lazy":false}';
+orawp.init(config);
